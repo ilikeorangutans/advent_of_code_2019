abfcf336dee2d5c6774513dfe1f0681344a2da5d
Jakob Külzer <jakob.kuelzer@gmail.com>
2019-12-05 10:16:46 -0500

    solved part one of day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,10 +1,64 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    let x = String::from("1,9,10,3,2,3,11,0,99,30,40,50");
+    let input = String::from("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0");
 
-    let parts: Vec<&str> = x.split(',').map(|s| s.trim()).collect();
+    let mut program: Vec<i64> = input
+        .split(',')
+        .map(|s| s.trim())
+        .map(|s| s.parse().expect("every value should be an integer"))
+        .collect();
 
-    for chunk in parts.chunks(4) {
-        println!("got {}", chunk[0]);
+    // Set PC 1 to 12 and PC 2 to 2
+    program[1] = 12;
+    program[2] = 2;
+
+    match run_program(program) {
+        Ok(updated_program) => println!("Result at 0: {}", updated_program[0]),
+        Err(message) => println!("execution error: {}", message),
+    }
+}
+
+fn run_program(mut program: Vec<i64>) -> Result<Vec<i64>, String> {
+    let mut pc = 0;
+    loop {
+        let opcode = match program.get(pc) {
+            None => {
+                return Err(format!(
+                    "at position {} expected opcode but found nothing",
+                    pc
+                ));
+            }
+            Some(val) => val,
+        };
+
+        pc = match opcode {
+            1 => {
+                let a = *program.get(pc + 1).unwrap() as usize;
+                let b = *program.get(pc + 2).unwrap() as usize;
+                let dst = *program.get(pc + 3).unwrap() as usize;
+                let val = program[a] + program[b];
+                program[dst] = val;
+                println!(
+                    "{} [{}] + {} [{}] = {} [{}]",
+                    a, program[a], b, program[b], dst, val
+                );
+                pc + 4
+            }
+            2 => {
+                let a = *program.get(pc + 1).unwrap() as usize;
+                let b = *program.get(pc + 2).unwrap() as usize;
+                let dst = *program.get(pc + 3).unwrap() as usize;
+                let val = program[a] * program[b];
+                program[dst] = val;
+                println!(
+                    "{} [{}] * {} [{}] = {} [{}]",
+                    a, program[a], b, program[b], dst, val
+                );
+                pc + 4
+            }
+            99 => return Ok(program),
+            _ => return Err("unknown opcode".to_string()),
+        }
     }
 }
